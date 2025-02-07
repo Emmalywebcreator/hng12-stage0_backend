6721ff98af85851935bf575335da679074d5be29
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-02-07 20:04:24 +0100

    updated the code to use ordered dict

--- stage_0_backend.py
+++ stage_0_backend.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify
 from datetime import datetime
 import pytz
+from collections import OrderedDict
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def get_info():
-    response = {
-        "email": "emmyprime2015@gmail.com",
-        "current_date": datetime.now(pytz.UTC).isoformat(),
-        "github_url": "https://github.com/Emmalywebcreator/"
-    }
+    response = OrderedDict([
+       ("email", "emmyprime2015@gmail.com"),
+       ( "current_date", datetime.now(pytz.UTC).isoformat()),
+       ( "github_url", "https://github.com/Emmalywebcreator/")
+    ])
     return jsonify(response), 200
 
 
