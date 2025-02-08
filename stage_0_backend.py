e26a59a49236b136e56c1d2fadf6b679fc240b2a
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-02-08 11:28:51 +0100

    Trying to use Response instead of ordered dict

--- stage_0_backend.py
+++ stage_0_backend.py
@@ -1,21 +1,23 @@
 from flask import Flask, Response
-from datetime import datetime
 import pytz
+from datetime import datetime
 import json
-from collections import OrderedDict
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def get_info():
-    response = OrderedDict([
-        ("email", "emmyprime2015@gmail.com"),
-        ("current_datetime", datetime.now(pytz.UTC).isoformat()),
-        ("github_url", "https://github.com/Emmalywebcreator/")
-    ])
+    response = {
+        "email": "emmyprime@gmail.com",
+        "current_datetime": datetime.now(pytz.UTC).isoformat(),
+        "github_url": "https://github.com/Emmalywebcreator"
+    }
     
-    # Use json.dumps() to manually serialize and set Content-Type
-    return Response(json.dumps(response), mimetype="application/json"), 200
+    return Response(
+        json.dumps(response, sort_keys=False),
+        mimetype="application/json"
+    ), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
